ba210ac3c0e7459c91446f94945d0a4ebd120327
yoshitugu <yuki.38220512@gmail.com>
2020-02-26 05:00:49 +0000

    finish top-page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+ # ページごとにタイトルを返す
+  def full_title(page_name = "")
+    base_title = "AttendanceApp"
+    if page_name.empty?
+      base_title
+    else
+      page_name + " | " + base_title
+    end
+  end
 end
