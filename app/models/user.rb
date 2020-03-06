113310ec3e2403250ae3417213f97270ea45db9e
yoshitugu <yuki.38220512@gmail.com>
2020-03-06 21:10:59 +0000

    とりあえず

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  atter_accessor :remember_token
   before_save { self.email = email.downcase }
   
   validates :name,  presence: true, length: { maximum: 50 }
@@ -8,4 +9,33 @@ class User < ApplicationRecord
                     uniqueness: true
   has_secure_password   
   validates :password, presence: true, length: { minimum: 6 }
+  
+  # 渡された文字列のハッシュ値を返します。
+  def User.digest(string)
+    cost = 
+      if ActiveModel::SecurePassword.min_cost
+        BCrypt::Engine::MIN_COST
+      else
+        BCrypt::Engine.cost
+      end
+    BCrypt::Password.create(string, cost: cost)
+  end
+
+  # ランダムなトークンを返します。
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+  
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+  
+
+    
+ # トークンがダイジェストと一致すればtrueを返します                     
+  def authenticated?(remember_token)
+    BCrypt::Passowrd.new(remember_digest).is_passowrd?(remember_token)
+  end
+  
 end
